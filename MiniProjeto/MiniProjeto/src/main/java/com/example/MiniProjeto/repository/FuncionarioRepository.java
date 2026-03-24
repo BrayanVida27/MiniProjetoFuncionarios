084daed35da6576563f81e8044aaf3669163a160
Brayan <brayandefariasmarcos@gmail.com>
2026-03-24 19:23:39 -0300

    Delete MiniProjeto/MiniProjeto directory

--- MiniProjeto/MiniProjeto/src/main/java/com/example/MiniProjeto/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MiniProjeto.repository;
-
-import com.example.MiniProjeto.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface FuncionarioRepository extends JpaRepository <Funcionario, Long> {
-}
